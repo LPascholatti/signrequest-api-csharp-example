5e11f62bb311a969cd87a7341a7233a97e1bbee7
LPascholatti <lucaspascholatti@gmail.com>
2021-05-06 13:00:15 +0200

    Initial commit with example

---
+++ SignrequestTest/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using SignRequest.Api;
+using SignRequest.Client;
+using SignRequest.Model;
+
+namespace SignrequestTest
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            await SignrequestTest();
+            await Base64Test();
+
+            Console.ReadLine();
+        }
+
+        private static async Task SignrequestTest()
+        {
+            var file = await File.ReadAllBytesAsync("./demo_document.pdf");
+            var pdfBase64 = Convert.ToBase64String(file);
+
+            var api = new SignrequestQuickCreateApi
+            {
+                Configuration = { BasePath = "https://{your_team}.signrequest.com/api/v1" }
+            };
+
+            api.Configuration.AddApiKey("Authorization", "YOUR_TOKEN_HERE");
+            api.Configuration.AddApiKeyPrefix("Authorization", "Token");
+
+            var data = new SignRequestQuickCreate(
+                signers: new List<Signer> { new("email@provider.com") },
+                fromEmail: "email+1@provider.com",
+                fileFromContent: pdfBase64,
+                fileFromContentName: "demo_document.pdf",
+                who: SignRequestQuickCreate.WhoEnum.O);
+
+            try
+            {
+                var result = await api.SignrequestQuickCreateCreateAsync(data);
+                Console.WriteLine("Success!");
+                Console.WriteLine(result);
+                Environment.Exit(0);
+            }
+            catch (ApiException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static async Task Base64Test()
+        {
+            var file = await File.ReadAllBytesAsync("./demo_document.pdf");
+            var pdfBase64 = Convert.ToBase64String(file);
+
+            var pdfBytes = Convert.FromBase64String(pdfBase64);
+            await File.WriteAllBytesAsync("./demo_document_dupe.pdf", pdfBytes);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# SignRequest Example Using C#
+
+## -> Using SignRequest Quick Create method /signrequest-quick-create/
+
+## -> Sending a file which is encoded to base64
+
+1. Make sure you have .net installed in your computer - More details in Microsoft's documentation.
+2. Install SignRequest's package using nuget - Find more details in SignRequest's documentation.
+3. Configure this code, adding your document replacing demo_document.pdf. Add your Token and subdomain.
+4. Run 'dotnet run'.
+5. The program will exit automatically, as it's using `Environment.Exit(0)`;
